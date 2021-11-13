8566f423f3a22d2dae26436e812821469ae0446d
rohankarankot <rohankarankot@gmail.com>
2021-11-13 19:59:42 +0530

    code refactored

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,9 @@ const app = express();
 
 app.use(express.json());
 // Available Routes
-// app.get("/", (req, res) => {
-//   res.json({ success: "home" });
-// });
+app.get("/", (req, res) => {
+  res.json({ success: "home" });
+});
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,11 +3,6 @@ const router = express.Router();
 const User = require("../modals/User");
 
 //Create a new User Using: POST "api/auth"  NOT require Auth
-router.post("/", (req, res) => {
-  res.send(req.body);
-  console.log(req.body);
-  const user = User(req.body);
-  user.save();
-});
+router.post("/", (req, res) => {});
 
 module.exports = router;
