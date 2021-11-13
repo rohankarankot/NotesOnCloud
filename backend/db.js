e9df967e04485e967eac2a569d8718fb958ad4c0
rohankarankot <rohankarankot@gmail.com>
2021-11-13 20:27:56 +0530

    validation added

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const mongoUrI =
-  "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+  "mongodb://localhost:27017/CloudNotesBackend?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
 
 const connectToMongoose = () => {
   mongoose.connect(mongoUrI, () => {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,31 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../modals/User");
-
+const { body, validationResult } = require("express-validator");
 //Create a new User Using: POST "api/auth"  NOT require Auth
-router.post("/", (req, res) => {});
+router.post(
+  "/",
+  [
+    body("email").isEmail(),
+    body("name").isLength({ min: 3 }),
+    body("password").isLength({ min: 5 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+      email: req.body.email,
+      name: req.body.name,
+      password: req.body.password,
+    })
+      .then((user) => res.json(user))
+      .catch((err) => {
+        console.log(err);
+        res.json({ errors: "Email already exists" });
+      });
+  }
+);
 
 module.exports = router;
