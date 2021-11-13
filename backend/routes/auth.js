091580ffff27d98517032107006de961fadd52b2
rohankarankot <rohankarankot@gmail.com>
2021-11-13 21:42:25 +0530

    created login endpoint

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -47,5 +47,44 @@ router.post(
     }
   }
 );
-
+//LOgin  a new User Using: POST "api/auth/login"  NOT require Auth
+router.post(
+  "/login",
+  [
+    body("email").isEmail(),
+    body("password", "Hmm...!!! Password cannot be blank").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const email = req.body.email;
+    const password = req.body.password;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(500)
+          .send({ error: "Login Failed.... Please try again." });
+      }
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res
+          .status(500)
+          .send({ error: "Login Failed.... Please try again." });
+      }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const awthToken = jwt.sign(data, JWT_SECRET);
+      res.send({ awthToken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send({ error: "internal server error" });
+    }
+  }
+);
 module.exports = router;
